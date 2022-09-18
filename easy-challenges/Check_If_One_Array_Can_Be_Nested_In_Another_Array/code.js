574a4fa27a03391e8a1841a178904810040c787d
xocaid <xocaid@gmail.com>
2022-09-17 17:16:04 -0700

    Commit solution

---
+++ easy-challenges/Check_If_One_Array_Can_Be_Nested_In_Another_Array/code.js
@@ -0,0 +1,36 @@
+//PROMPT
+/*
+ Create a function that returns true if the first array can be nested inside the second.
+arr1 can be nested inside arr2 if:
+arr1's min is greater than arr2's min.
+arr1's max is less than arr2's max.*/
+
+function canNest(arr1, arr2) {
+  let arr1min = arr1[0];
+  let arr1max = arr1[arr1.length - 1];
+  let arr2min = arr2[0];
+  let arr2max = arr2[arr2.length - 1]
+
+  if (arr1min > arr2min && arr1max < arr2max) {
+    return true;
+  }
+  return false;
+}
+function canNest(arr1, arr2) {
+  let arr1Min = Math.min(...arr1);
+  let arr1Max = Math.max(...arr1);
+
+  let arr2Min = Math.min(...arr2);
+  let arr2Max = Math.max(...arr2);
+
+  if (arr1Min > arr2Min && arr1Max < arr2Max) {
+    return true;
+  } return false;
+  //This works because Math.min & Math.max will go through array and find the min & max, this is specifically for example 2. Solution can also be written:
+  //return Math.min(...arr1) > Math.min(...arr2) && Math.max(...arr1) < Math.max(...arr2)
+}
+
+console.log(canNest([1, 2, 3, 4], [0, 6]), "should return true")
+console.log(canNest([3, 1], [4, 0]), "should return true")
+console.log(canNest([9, 9, 8], [8, 9]), "should return false")
+console.log(canNest([1, 2, 3, 4], [2, 3]), "should return false")
