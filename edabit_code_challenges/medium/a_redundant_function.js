70a9de87e1491ec9f96651feeb1a2932d9449b24
Xochitl <xocaid@gmail.com>
2023-12-13 18:53:36 -0800

    adding medium folders

---
+++ edabit_code_challenges/medium/a_redundant_function.js
@@ -0,0 +1,11 @@
+//PROMPT
+/*
+Write a function redundant that takes in a string str and returns a function that returns str.
+*/
+function redundant(str) {
+	
+}
+
+console.log(redundant("apple") )
+console.log(redundant("pear"))
+console.log(redundant(""))
